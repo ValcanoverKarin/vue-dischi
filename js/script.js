50446f7c0d648a77ab7806f061dbc615f01d1b60
karin valcanover <valcanoverkarin@outlook.it>
2021-04-28 17:10:30 +0200

    sostituito le immagini

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ var app = new Vue (
     {
         el: '#root',
         data: {
-            
+            //1a. creo variabile vuota
+            cds: []
         },
         methods: {
-
         },
-        mounted() {}
+        mounted() {
+            //1b. faccio la richiesta
+            axios
+            //1c. aggiungo l' apposita API di Boolea
+            .get('https://flynn.boolean.careers/exercises/api/array/music ')
+            //1d. creo funzione
+            .then( (response) => {
+                const result = response.data;
+                this.cds = result.response;
+            });
+        }
     }
 );
